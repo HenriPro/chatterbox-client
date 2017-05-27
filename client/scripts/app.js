c4122c74a823d3016e8ddfe2ce37067be25bdccf
Hack Reactor Student <student@hackreactor.com>
2017-05-26 20:03:03 -0700

    post kinda working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,12 @@
 // YOUR CODE HERE:
 var app = {};
+
 app.init = function(){
-  this.handleUsernameClick();
-  this.handleSubmit();
+  //console.log("here");
+  // this.handleUsernameClick();
+  // this.handleSubmit();
+  this.fetch();
+  // this.attachEventHandlers();
 };
 
 app.server = "http://parse.sfm6.hackreactor.com/chatterbox/classes/messages";
@@ -10,12 +14,15 @@ app.server = "http://parse.sfm6.hackreactor.com/chatterbox/classes/messages";
 app.send = function(message) {
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    //xhrFields: {cors: false},
+    url: app.server,
     type: 'POST',
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message sent');
+
+
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -27,12 +34,15 @@ app.send = function(message) {
 app.fetch = function() {
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    url: app.server,
     type: 'GET',
-    data: JSON.stringify(message),
+    // data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message recieved');
+      console.log(data);
+      app.renderAll(data.results);
+
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -47,10 +57,10 @@ app.clearMessages = function() {
 };
 
 app.renderMessage = function(message) {
-  var user = $("<h1 class='user'></h1>");
+  var user = $("<p class='username'></p>");
   var text = $("<p class='text'></p>");
   var roomname = $("<p class='user'></p>");
-  var messageDiv = $("<div class='message'></div>");
+  var messageDiv = $("<div class='chat'></div>");
   messageDiv.append(user, text, roomname);
   $('#chats').append(messageDiv);
   //$('.user').textContent = message.username;
@@ -59,6 +69,14 @@ app.renderMessage = function(message) {
   $(roomname).text(message.roomname);
   //element.textContent = "<%=untrustedData%>";
 
+};
+
+app.renderAll = function(array) {
+  //console.log(array.results);
+  for (var i = 0; i < array.length; i++) {
+    // console.log(array[i]);
+    this.renderMessage(array[i]);
+  }
 };
 
 app.renderRoom = function(roomName) {
@@ -70,6 +88,21 @@ app.handleUsernameClick = function() {
 
 };
 
-app.handleSubmit = function() {
+// app.handleSubmit = function() {
+//   var value = $('#messageinput').val();
+//   console.log(typeof value);
+//   app.send(value);
+//
+// };
+//
+// app.attachEventHandlers = function() {
+//   // document.getElementById('sendbutton').addEventListener('click', this.handleSubmit.bind(this));
+//   $(function() {
+//     $("#sendbutton").click(function( ) {
+//       app.handleSubmit();
+//     });
+//   });
 
-};
+// }
+
+app.init();
